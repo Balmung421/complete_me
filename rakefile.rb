356f3486d95385b7ea552534e0a0910d466871a7
Balmung421 <zcook421@gmail.com>
2017-07-12 14:13:37 -0600

    Rename files

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
 task :default do
-    FileList["./test/node_test.rb", "./test/trie_test.rb", "./test/complete_me_spec_harness.rb"].each { |file| ruby file } #do/end on one line!!!
+    FileList["./test/node_test.rb", "./test/complete_me_test.rb", "./test/complete_me_spec_harness.rb"].each { |file| ruby file } #do/end on one line!!!
     #just type rake in your directory where your rakefile.rb is
 end
